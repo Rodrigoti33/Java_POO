417754e3dee393202826213a63d8dae2fdeec718
Renan Germano <61218420+renangfs@users.noreply.github.com>
2024-03-07 15:41:01 -0300

    Add files via upload

---
+++ Exercicio 2/src/Casting.java
@@ -0,0 +1,14 @@
+public class Casting {
+    public static void main(String[] args){
+
+        double valordouble = 5.6;
+        int valorint;
+
+        valorint = (int) valordouble;
+
+        System.out.println(valorint);
+
+
+
+    }
+}
